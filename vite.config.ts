1017e0631d585c9869d536768c529f423b90ce3e
Megan Wang <115515191+rebootMegan@users.noreply.github.com>
2024-06-01 22:56:31 -0400

    push new code

--- vite.config.ts
+++ vite.config.ts
@@ -45,13 +45,13 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 			port: env.VITE_PORT as unknown as number,
 			open: JSON.parse(env.VITE_OPEN),
 			proxy: {
-				"/api": {
-					target: 'https://explorease-test2-58ca368c968d.herokuapp.com/', // 代理的目标地址
-					changeOrigin: true, // 开发模式，默认的origin是真实的 origin:localhost:3000 代理服务会把origin修改为目标地址
-					// secure: true, // 是否https接口
-					// ws: true, // 是否代理websockets
-					rewrite: (path) => path.replace(/^\/api/, '') // 路径重写
-				}
+				// "/api": {
+				// 	target: 'http://127.0.0.1:8089', // 代理的目标地址
+				// 	changeOrigin: true, // 开发模式，默认的origin是真实的 origin:localhost:3000 代理服务会把origin修改为目标地址
+				// 	// secure: true, // 是否https接口
+				// 	// ws: true, // 是否代理websockets
+				// 	rewrite: (path) => path.replace(/^\/api/, '') // 路径重写
+				// }
 			}
 		}
 	};
